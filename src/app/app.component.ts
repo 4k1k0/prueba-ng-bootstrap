eb64a6c382bbc64cfdd3ed6619596ccb0819995f
Wako <dulo.rodrigo@gmail.com>
2019-06-06 09:51:11 -0500

    Prueba lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pruebaCalendario';
+export class AppComponent implements OnInit {
+
+  fechaHoy: any;
+  haceUnAno: any;
+  model: NgbDateStruct;
+
+  constructor(private calendar: NgbCalendar) {}
+
+  ngOnInit() {
+    this.obtenerHoy();
+  }
+
+  obtenerHoy() {
+    this.seleccionarFecha();
+    const a = moment().format('YYYY/MM/DD');
+    const b = moment().subtract(1, 'year').format('YYYY/MM/DD');
+    this.fechaHoy = this.generaFecha(a);
+    this.haceUnAno = this.generaFecha(b);
+  }
+
+  generaFecha(f) {
+    const datos = f.split('/');
+    return {
+      year: parseInt(datos[0]),
+      month: parseInt(datos[1]),
+      day: parseInt(datos[2])
+    }
+  }
+
+  seleccionarFecha() {
+    this.model = this.calendar.getToday();
+  }
+
 }
